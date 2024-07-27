438ad3e1bc8955a82d9d449a487955f384fe3be2
D F <dmitrifedoruk@gmail.com>
2024-07-27 10:15:10 -0400

    Added month and year select

--- main.js
+++ main.js
@@ -4,20 +4,151 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div>
     <div id="wrapper">
+        <div id="navBar">
+            <div id="leftButton" class="menuButton"></div>
+            <div id="monthAndYearMenu">
+                <div id="monthMenu">
+                    <label class="menuLabel">Month: </label>
+                    <select id="monthSelect"></select>
+                </div>
+                <div id="yearMenu">
+                    <label class="menuLabel">Year: </label>
+                    <select id="yearSelect"></select>
+                </div>
+                <button id="submit">Go</button>
+            </div>
+            <div id="rightButton" class="menuButton"></div>
+        </div>
         <div id="monthHeading"><h1 id="month"></h1><h1 id="year"></h1></div>
         <div id="weekdayHeading"></div>
         <div id="dateGrid"></div>
-
-
-
-
     </div>
 
   </div>
 `
 
-//arrays of weekday names
-const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+//generates string representing current date
+function dateString(month, day, year) {
+    return (month+1).toString()+"-"+day.toString()+"-"+year.toString()
+}
+
+function showMenu() {
+    const menu = document.querySelector("#monthAndYearMenu");
+    if (menu.classList[0] === "showMenu") {
+        menu.classList.remove("showMenu");
+        menu.classList.add("hideMenu");
+    } else {
+        menu.classList.remove("hideMenu");
+        menu.classList.add("showMenu");
+    }
+}
+
+document.querySelector("#rightButton").addEventListener("click",showMenu);
+
+function createCalendar(date) {
+
+    //current date to compare for highlighting
+    const now = new Date();
+    const currentDay = now.getDate();
+    const currentMonth = now.getMonth();
+    const currentYear = now.getFullYear();
+
+    //data to generate calendar dates
+    const month =  date.getMonth();
+    const year = date.getFullYear();
+
+    //option to extract month name from date
+    const options = { month: "long" };
+    const monthName = new Intl.DateTimeFormat("en-US", options).format(date);
+    document.querySelector('#month').innerHTML = monthName;
+
+    //set year on display
+    document.querySelector('#year').innerHTML = year.toString();
+
+    document.querySelector("title").innerHTML = monthName + " " + year.toString();
+
+
+    //number of days in current month
+    const daysInMonth = (new Date(year,month + 1,0).getDate());
+
+    //number of days in prior month (to determine what to display at beginning on calendar)
+    const daysInPriorMonth = (new Date(year,month,0).getDate());
+
+    //finds the day of the week that is the first day of the month
+    const firstDayOfWeek = new Date(year,month,8).getDay();
+
+    const priorMonthDaysStart = daysInPriorMonth - (firstDayOfWeek - 1) ;
+
+    //establishes an index to determine what range to offset the listed dates based on what day is the first
+    //of the month
+    const firstIndex = firstDayOfWeek + 1;
+    const lastIndex = firstDayOfWeek + daysInMonth;
+
+    let priorCountUp = priorMonthDaysStart;
+    let count = 1;
+    let postCount = 1;
+
+    //generates dates for grid inserting correct date numbers for each
+    for(let i = 1; i < 36; i++){
+        let dateBlock = document.createElement("div");
+        dateBlock.classList.add("dateBlock");
+
+        if(i < firstIndex){
+            let number = document.createElement("div");
+            number.inert = true;
+            number.innerText = priorCountUp.toString();
+            number.classList.add("priorAndNextMonth");
+            dateBlock.appendChild(number);
+            dateBlock.setAttribute("data-date",dateString(month,count,year));
+            priorCountUp++;
+        }
+
+        else if(i >= firstIndex && i <= lastIndex){
+            let number = document.createElement("div");
+            number.inert = true;
+            number.innerText = count.toString();
+            dateBlock.appendChild(number);
+            dateBlock.setAttribute("data-date",dateString(month,count,year));
+            if(Date.parse(dateString(month,count,year)) === Date.parse(dateString(currentMonth,currentDay,currentYear))){
+                dateBlock.classList.add("todayBlock");
+            }
+            count++;
+        }
+
+        else if(i > lastIndex){
+            let number = document.createElement("div");
+            number.inert = true;
+            number.innerText = postCount.toString();
+            number.classList.add("priorAndNextMonth");
+            dateBlock.appendChild(number);
+            dateBlock.setAttribute("data-date",dateString(month,count,year));
+            postCount++;
+        }
+
+        dateBlock.addEventListener("click",function (e){
+            this.classList.toggle("highlighted")
+        });
+
+        document.querySelector('#dateGrid').appendChild(dateBlock);
+    }
+
+}
+
+
+function updateCalendar() {
+
+    const month = document.querySelector("#monthSelect").value;
+    const year = document.querySelector("#yearSelect").value;
+
+    const newDate = new Date(month+"-1-"+year.toString());
+
+    document.querySelector("#dateGrid").replaceChildren();
+
+    createCalendar(newDate);
+}
+
+document.querySelector("#submit").addEventListener("click",updateCalendar);
+
 
 //current date
 const today = new Date();
@@ -27,20 +158,11 @@ const month =  today.getMonth();
 const year = today.getFullYear();
 
 
-function dateString(month, day, year) {
-    return (month+1).toString()+"-"+day.toString()+"-"+year.toString()
-}
+const months = ['January','February','March','April','May','June','July',
+    'August','September','October','November','December'];
 
-//option to extract month name from date
-const options = { month: "long" };
-const monthName = new Intl.DateTimeFormat("en-US", options).format(today);
-document.querySelector('#month').innerHTML = monthName;
-
-//set year on display
-document.querySelector('#year').innerHTML = year.toString();
-
-document.querySelector("title").innerHTML = monthName + " " + year.toString();
-
+//arrays of weekday names
+const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 //generates headings for each weekday "column" using array
 weekdays.forEach((element) => {
@@ -50,68 +172,24 @@ weekdays.forEach((element) => {
     document.querySelector('#weekdayHeading').appendChild(weekday);}
 );
 
-//number of days in current month
-const daysInMonth = (new Date(today.getFullYear(),today.getMonth() + 1,0).getDate());
 
-//number of days in prior month (to determine what to display at beginning on calendar)
-const daysInPriorMonth = (new Date(today.getFullYear(),today.getMonth(),0).getDate());
+//populates month select options from array
+months.forEach((element) => {
+    let monthOption = document.createElement("option");
+    // weekday.classList.add("weekday");
+    monthOption.innerText = element;
+    document.querySelector('#monthSelect').appendChild(monthOption);}
+);
 
-//finds the day of the week that is the first day of the month
-const firstDayOfWeek = new Date(today.getFullYear(),today.getMonth(),8).getDay();
-
-const priorMonthDaysStart = daysInPriorMonth - (firstDayOfWeek - 1) ;
-
-//establishes an index to determine what range to offset the listed dates based on what day is the first
-//of the month
-const firstIndex = firstDayOfWeek + 1;
-const lastIndex = firstDayOfWeek + daysInMonth;
-
-let priorCountUp = priorMonthDaysStart;
-let count = 1;
-let postCount = 1;
-
-
-//generates dates for grid inserting correct date numbers for each
-for(let i = 1; i < 36; i++){
-    let dateBlock = document.createElement("div");
-    dateBlock.classList.add("dateBlock");
-
-    if(i < firstIndex){
-        let number = document.createElement("div");
-        number.inert = true;
-        number.innerText = priorCountUp.toString();
-        number.classList.add("priorAndNextMonth");
-        dateBlock.appendChild(number);
-        dateBlock.setAttribute("data-date",dateString(month,count,year));
-        priorCountUp++;
-    }
-    else if(i >= firstIndex && i <= lastIndex){
-        let number = document.createElement("div");
-        number.inert = true;
-        number.innerText = count.toString();
-        dateBlock.appendChild(number);
-        dateBlock.setAttribute("data-date",dateString(month,count,year));
-        if(Date.parse(dateString(month,count,year)).valueOf() === Date.parse(dateString(month, day, year))){
-            dateBlock.classList.add("todayBlock");
-        }
-        count++;
-    }
-    else if(i > lastIndex){
-        let number = document.createElement("div");
-        number.inert = true;
-        number.innerText = postCount.toString();
-        number.classList.add("priorAndNextMonth");
-        dateBlock.appendChild(number);
-        dateBlock.setAttribute("data-date",dateString(month,count,year));
-        postCount++;
-    }
-    dateBlock.addEventListener("click",function (e){
-        this.classList.toggle("highlighted")
-    });
-    document.querySelector('#dateGrid').appendChild(dateBlock);
+//populates year select options from current year back to 1969
+for(let i = year; i >= 1969; i--) {
+    let yearOption = document.createElement("option");
+    // weekday.classList.add("weekday");
+    yearOption.innerText = i.toString();
+    document.querySelector('#yearSelect').appendChild(yearOption);
 }
 
-
+createCalendar(today);
 
 
 
